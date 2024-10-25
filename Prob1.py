501ce558439c2da130254216217033e73f203f10
suzanne1207 <sgunderson@willamette.edu>
2024-10-25 14:20:39 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -2,7 +2,19 @@ from pgl import *
 
 #1a
 def create_histogram_array(data:list[int])->list[int]:
-    pass
+    HISTOGRAM = [0,1,2,3,4,5,6,7,8,9]
+    for i in HISTOGRAM: # examines each number in the HISTOGRAM list
+        count = 0 # sets count to zero each time so it's specific to each number
+        if i in PI_DIGITS: # if the number is in pi
+            for j in PI_DIGITS: # examines each number in pi
+                if j == i: # if number in pi is in histogram
+                    count += 1 # adds one to the count for that number
+        else: # if the number is not in pi,
+            count = 0 # count stays at zero
+        HISTOGRAM[i] = count # replaces the number with its count
+    print(HISTOGRAM) # prints the HISTOGRAM list
+         
+
 
 #1b
 def print_histogram(hist:list[int]) -> None:
