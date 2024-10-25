73b85e0d1ba9567f7768b8aa7ca29900b0186587
suzanne1207 <sgunderson@willamette.edu>
2024-10-25 15:48:16 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,24 +1,29 @@
 from pgl import *
 
 #1a
-def create_histogram_array(data:list[int])->list[int]:
+def create_histogram_array(pi):
     HISTOGRAM = [0,1,2,3,4,5,6,7,8,9]
     for i in HISTOGRAM: # examines each number in the HISTOGRAM list
         count = 0 # sets count to zero each time so it's specific to each number
-        if i in PI_DIGITS: # if the number is in pi
-            for j in PI_DIGITS: # examines each number in pi
+        if i in pi: # if the number is in pi
+            for j in pi: # examines each number in pi
                 if j == i: # if number in pi is in histogram
                     count += 1 # adds one to the count for that number
         else: # if the number is not in pi,
             count = 0 # count stays at zero
         HISTOGRAM[i] = count # replaces the number with its count
-    print(HISTOGRAM) # prints the HISTOGRAM list
+    return HISTOGRAM
          
-
-
 #1b
-def print_histogram(hist:list[int]) -> None:
-    pass
+def print_histogram(hist):
+    number = -1 # count starts at -1 so 1 can be added each time
+    for i in hist: # for each value in the histogram list,
+        asterisks = "*" * i # the asterisks are equal to i
+        number += 1 # the number increases by one each time
+        ASTERISKS = str(asterisks) # turns asterisks into a string
+        NUMBER = str(number) # turns number into a string
+        print(NUMBER + ": " + ASTERISKS) # prints histogram
+
 
 #1c
 def graph_histogram(hist:list[int], width:int, height:int) -> None:
@@ -35,6 +40,6 @@ def graph_histogram(hist:list[int], width:int, height:int) -> None:
 PI_DIGITS = [3,1,4,1,5,9,2,6,5,3,5,5,8,9,7,9]
 hist = create_histogram_array(PI_DIGITS)
 print(hist)
-#print_histogram(hist) # uncomment to test
+print_histogram(hist) # uncomment to test
 #graph_histogram(hist, 400, 400) # uncomment to test
 
